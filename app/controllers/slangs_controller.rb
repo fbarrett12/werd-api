6a956e2c2ca2f891d91c6a4826aa7cc57dce911d
FredrickBarrett <f.barrett12@gmail.com>
2020-02-07 21:37:05 -0500

    added serializers

--- app/controllers/slangs_controller.rb
+++ app/controllers/slangs_controller.rb
@@ -1,14 +1,17 @@
 class SlangsController < ApplicationController
     def index
         @slangs = Slang.all
+        render json: SlangSerializer.new(@slangs).serializable_hash
     end
 
     def create
         @slang = Slang.create(slang_params)
+        render json: SlangSerializer.new(@slangs).serializable_hash
     end
 
     def show
         @slang = Slang.find(params[:id])
+        render json: SlangSerializer.new(@slangs).serializable_hash
     end
 
     private
