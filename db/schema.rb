86ac1f1ac8f3b89cc4901b4b5d7b71b80a54c09e
FredrickBarrett <f.barrett12@gmail.com>
2020-02-07 22:08:37 -0500

    added choices model, controller, and serializers

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_012509) do
+ActiveRecord::Schema.define(version: 2020_02_08_030450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "choices", force: :cascade do |t|
+    t.text "definition"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "slangs", force: :cascade do |t|
     t.text "term"
