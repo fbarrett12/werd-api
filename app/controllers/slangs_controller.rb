5d69cfe4bc411a3bb3d867bb2f468873de49ac41
FredrickBarrett <f.barrett12@gmail.com>
2020-02-07 20:51:30 -0500

    rails g Resource slang, migrated, and also added index and create methods in slang controller

--- app/controllers/slangs_controller.rb
+++ app/controllers/slangs_controller.rb
@@ -1,2 +1,16 @@
 class SlangsController < ApplicationController
+    def index
+        @slangs = Slang.all
+    end
+
+    def create
+        @slang = Slang.create(slang_params)
+    end
+
+    private
+
+    def slang_params
+        params.permit(:term, :definition)
+    end
 end
+
